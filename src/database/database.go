a548b96e83e87657d617d8a5681aad684951f35f
LucianoBrumer <lucianobrumer5@gmail.com>
2023-10-03 19:47:06 -0300

    rename env variable url to server

--- src/database/database.go
+++ src/database/database.go
@@ -18,17 +18,17 @@ func Connect() {
 	}
 
 	DB_INFO := map[string]interface{}{
-		"USER": os.Getenv("DB_USER"),
-		"PASS": os.Getenv("DB_PASS"),
-		"URL":  os.Getenv("DB_URL"),
-		"PORT": os.Getenv("DB_PORT"),
-		"NAME": os.Getenv("DB_NAME"),
+		"USER":   os.Getenv("DB_USER"),
+		"PASS":   os.Getenv("DB_PASS"),
+		"SERVER": os.Getenv("DB_SERVER"),
+		"PORT":   os.Getenv("DB_PORT"),
+		"NAME":   os.Getenv("DB_NAME"),
 	}
 
 	dsn := fmt.Sprintf("DESKTOP-LK9AQTD://%s:%s@%s:%s?database=%s",
 		DB_INFO["USER"].(string),
 		DB_INFO["PASS"].(string),
-		DB_INFO["URL"].(string),
+		DB_INFO["SERVER"].(string),
 		DB_INFO["PORT"].(string),
 		DB_INFO["NAME"].(string),
 	)
