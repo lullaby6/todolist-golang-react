2a33be297066a5bacb1c57132288083224325940
LucianoBrumer <lucianobrumer5@gmail.com>
2023-10-02 21:13:04 -0300

    trying connect sql server...

--- src/database/database.go
+++ src/database/database.go
@@ -1,17 +1,43 @@
 package database
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+var DB_INFO = map[string]interface{}{
+	"USER": os.Getenv("DB_USER"),
+	"PASS": os.Getenv("DB_PASS"),
+	"URL":  os.Getenv("DB_URL"),
+	"PORT": os.Getenv("DB_PORT"),
+	"NAME": os.Getenv("DB_NAME"),
+}
+
 func Connect() {
-	dsn := "sqlserver://admin:admin@localhost:1433?database=todos"
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file.")
+	}
+
+	dsn := fmt.Sprintf("sqlserver://%s:%s@%s:%s?database=%s",
+		DB_INFO["USER"].(string),
+		DB_INFO["PASS"].(string),
+		DB_INFO["URL"].(string),
+		DB_INFO["PORT"].(string),
+		DB_INFO["NAME"].(string),
+	)
+
+	fmt.Println(dsn)
+
 	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("Failed to connect database.")
+		panic("Failed to connect database (" + dsn + ").")
 	}
 	DB = db
 }
