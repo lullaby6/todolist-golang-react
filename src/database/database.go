99017ac1e9afe59238d8bcd41ef46c3cb96f1d83
LucianoBrumer <lucianobrumer5@gmail.com>
2023-10-03 22:06:45 -0300

    refactoring database.go

--- src/database/database.go
+++ src/database/database.go
@@ -16,20 +16,12 @@ func Connect() {
 		panic(fmt.Sprintf("[ERROR] Problem loading .env file (%s).", err))
 	}
 
-	DB_INFO := map[string]interface{}{
-		"USER":   os.Getenv("DB_USER"),
-		"PASS":   os.Getenv("DB_PASS"),
-		"SERVER": os.Getenv("DB_SERVER"),
-		"PORT":   os.Getenv("DB_PORT"),
-		"NAME":   os.Getenv("DB_NAME"),
-	}
-
 	dsn := fmt.Sprintf("DESKTOP-LK9AQTD://%s:%s@%s:%s?database=%s",
-		DB_INFO["USER"].(string),
-		DB_INFO["PASS"].(string),
-		DB_INFO["SERVER"].(string),
-		DB_INFO["PORT"].(string),
-		DB_INFO["NAME"].(string),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_SERVER"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
 	)
 
 	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
